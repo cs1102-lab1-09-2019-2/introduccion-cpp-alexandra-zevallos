a6965720955732a56ec47a8ec3ec07fe4483637f
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-19 18:22:36 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using std::cout;
-
+int i;
 int main() {
-    for (int i = 0; i < ; i++) {
+    int i;
+    for (int i = 0; i < 10 ; i++) {
         //imprimir los 10 numeros [0-9]
+        cout<< i;
     }
+    return 0;
 }
