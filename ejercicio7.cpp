f56767e45fe700a6e2dd8baafacf5307250407d1
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-19 19:51:27 -0500

    Update ejercicio7.cpp

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -2,8 +2,10 @@
 using std::cout;
 
 int main() {
-         a = true; // declarar tipo de dato
+     bool a;
+     a = true; // declarar tipo de dato
      if (a) {
          cout << "Bien!!!";
      }
+     return 0;
 }
