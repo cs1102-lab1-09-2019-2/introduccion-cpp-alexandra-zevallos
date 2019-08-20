a7d1f10afb885f43d293f4209c8f196ca9fb6306
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-19 19:16:34 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -8,3 +8,16 @@ int main() {
         // Imprimir los numeros
     }
 }
+//No sabia porque colocaba vector<int>a
+//Cree un vector(array):
+#include <iostream>
+using std::cout;
+
+int main() {
+    int a;
+    int vector[5]={1, 2, 3, 4, 5};
+    for (int a=0; a<5; a++) {
+      cout<< vector[a]<<"\t";
+    } 
+    return 0;
+}
