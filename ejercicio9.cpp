89d249caef7a861dc91560defab7bfbebbe50295
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-22 10:17:00 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
-using std::cout;
 
 int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (i % 2 == 0) {
+        if () {
             //imprimir numeros pares verticalmente
-            cout<< i<<"\n";
         }
-        ++i;
+        
     }
   return 0;
 }
+
+
+//Modificaciones
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    int i=0;
+    while(i <= 10) {
+        if(i % 2 == 0) {
+            cout<< i<< "\t";
+        }
+        ++i;
+    }
+    return 0;
+}
+        
+       
