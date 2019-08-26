a41a1e05c3af504141b66a701649a2bdd61ad2c1
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-25 21:48:47 -0500

    Ejercicio 6 modificado

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,16 +1,3 @@
-
-int main() {
-    vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
-    for (auto a : v) {
-        for (auto i : a) {
-            // imprimir i
-        }
-        // salto de linea
-    }
-}
-
-
-//Modificaciones
 #include <iostream>
 #include <vector>
 
