238bb5e026c6d6c39676ca0c6e6c2b1ae18348df
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-25 22:00:31 -0500

    Ejercicio 8 modificado

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
 
-int main() {
-    auto i = 0;
-    // Mostrar los numeros del 0 al 4
-    while () {
-       
-    }
-    return 0;
-}
-
-
-//Modificaciones
-#include <iostream>
-
 using std::cout;
 
 int main() {
