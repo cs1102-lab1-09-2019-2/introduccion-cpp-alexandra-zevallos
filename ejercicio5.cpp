4f689d1c175d1b9d7273c465d2fd17b7f8f3795b
alexandra-zevallos <54292772+alexandra-zevallos@users.noreply.github.com>
2019-08-25 21:47:44 -0500

    Ejercicio 5 modificado

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
-using std::cout;
-
-int main() {
-
-    vector<int> a {1, 2, 3, 4, 5};
-    for (auto i : a) {
-        // Imprimir los numeros
-    }
-    return 0;
-}
-
-
-//Modificaciones
-#include <iostream>
 #include <vector>
 using std::cout;
 using std::vector;
